c831a5a3254658bfaf2d946194be9fce95ebdc54
homdreen <fehmel@gmail.com>
2018-07-12 18:11:16 -0300

    Checar funcoes de vitoria

--- main.py
+++ main.py
@@ -6,17 +6,42 @@
 #             https://github.com/homdreen/TicTacToe               #
 #                                                                 #   
 #                                                                 #
-#                  FELIPE HOMRICH MELCHIOR                        #
-#                        161150758                                #
+#                        REFERÊNCIA                               #
+#                    https://goo.gl/sph4pp                        #
+#                                                                 #
+#                    FELIPE HOMRICH MELCHIOR                      #
+#                          161150758                              #
 ################################################################### 
+# PSEUDOCÓDIGO                                                    #
+# function alphabeta(node, depth, α, β, maximizingPlayer) is      #
+#     if depth = 0 or node is a terminal node then                #
+#         return the heuristic value of node                      #
+#     if maximizingPlayer then                                    #
+#         v := -∞                                                 #
+#         for each child of node do                               #
+#             v := max(v, alphabeta(child, depth – 1, α, β,FALSE))#
+#             α := max(α, v)                                      #
+#             if α ≥ β then                                       #
+#                 break (* β cut-off *)                           #
+#         return v                                                #
+#     else                                                        #
+#         v := +∞                                                 #
+#         for each child of node do                               #
+#             v := min(v, alphabeta(child, depth – 1, α, β, TRUE))#
+#             β := min(β, v)                                      #  
+#             if α ≥ β then                                       #
+#                break (* α cut-off *)                            #
+#         return v                                                #
+###################################################################
 
 from tictactoe import * # Importação do arquivo tictactoe, que possui a classe principal
 
 if __name__ == '__main__': # Funcao que funciona como uma main
     game = TicTacToe() # Instanciação da classe TicTacToe
-    game.start()
+    #game.minimax(game.initialState, -1000, 1000, True)
     #game.printArguments()
-    #game.verifyWinRightDiag(game.initialState, game.player)
+    game.verifyWinCondition(game.initialState)
     #game.printState(game.initialState)
     #game.getStates(game.player)
     #game.generateStates()
+    
--- tictactoe.py
+++ tictactoe.py
@@ -15,9 +15,8 @@ class TicTacToe():
         self.opponent = 1 if self.player == 2 else 2
         self.k = int(sys.argv[4])
         self.initialState = [int(i) for i in sys.argv[5:]]
-        self.listMoves = []
-        self.gameStates = []
-        self.edgeStates = {}
+        self.play = []
+        self.depth = -1
         self.maxDepth = 15
 
         self.initialState = np.reshape(self.initialState, (self.m, self.n))
@@ -50,11 +49,11 @@ class TicTacToe():
                     sequence = 0  
                 if(sequence == self.k):
                     return 1
+            sequence = 0
         return 0
     
     def verifyWinRightDiag(self, state, player):
         sequence = 0
-
         auxRow = 0
         auxCol = 0
 
@@ -76,7 +75,6 @@ class TicTacToe():
 
     def verifyWinLeftDiag(self, state, player):
         sequence = 0
-
         auxRow = 0
         auxCol = 0
 
@@ -99,44 +97,80 @@ class TicTacToe():
 
     def verifyWinCondition(self, state):
         if(self.verifyWinX(state, self.player) == 1):
-            print("Jogador Ganhou")
+            print("Jogador Ganhou - X")
             return 1
         if(self.verifyWinY(state, self.player) == 1):
-            print("Jogador Ganhou")
+            print("Jogador Ganhou - Y")
             return 1
         if(self.verifyWinRightDiag(state, self.player) == 1):
-            print("Jogador Ganhou")
+            print("Jogador Ganhou - DiagRight")
             return 1
         if(self.verifyWinLeftDiag(state, self.player) == 1):
-            print("Jogador Ganhou")
+            print("Jogador Ganhou - DiagLeft")
             return 1
 
         if(self.verifyWinX(state, self.opponent) == 1):
-            print("Oponente Ganhou")
+            print("Oponente Ganhou - X")
             return -1
         if(self.verifyWinY(state, self.opponent) == 1):
-            print("Oponente Ganhou")
+            print("Oponente Ganhou - Y")
             return -1
         if(self.verifyWinRightDiag(state, self.opponent) == 1):
-            print("Oponente Ganhou")
+            print("Oponente Ganhou - DiagRight")
             return -1
         if(self.verifyWinLeftDiag(state, self.opponent) == 1):
-            print("Oponente Ganhou")
+            print("Oponente Ganhou - DiagLeft")
             return -1
 
-        print("Empate")
-        return 2
+        drawCount = 0
+        for i in range(self.m):
+            for j in range(self.n):
+                if(state[i][j] != 0):
+                    drawCount += 1
+        if(drawCount == len(state)):
+            print("Empate")
+            return 2
+
+        return 3
 
     def getMoves(self, state):
+        listMoves = []
         for i in range(self.m):
             for j in range(self.n):
                 if(state[i][j] == 0):
-                    self.listMoves.append([i,j])
+                    listMoves.append([i,j])
+        return listMoves
 
-    def start(self):
-        self.getMoves(self.initialState)
-
-        print(self.verifyWinCondition(self.initialState))
-
-        for i in self.listMoves:
-            print(i)
+    def minimax(self, state, alpha, beta, isMaxizingPlayer):
+        self.depth += 1
+        if(self.verifyWinCondition(state) == 3 and self.depth == self.maxDepth):
+            listMoves = self.getMoves(state)
+            if(len(listMoves) > 1):
+                if isMaxizingPlayer == True:
+                    bestScore = -1000
+                    for move in listMoves:
+                        currentState = state.copy()
+                        currentState[move[0]][move[1]] = self.player
+                        score = self.minimax(currentState, alpha, beta, False)
+                        if(score >= bestScore):
+                            self.play = move
+                            bestScore = score
+                            alpha = bestScore
+                        if(alpha >= beta):
+                            break # poda
+                else:
+                    bestScore = 1000
+                    for move in listMoves:
+                        currentState = state.copy()
+                        currentState[move[0]][move[1]] = self.opponent
+                        score = self.minimax(currentState, alpha, beta, True)
+                        if(score <= bestScore):
+                            self.play = move
+                            bestScore = score
+                            alpha = bestScore
+                        if(alpha >= beta):
+                            break  # poda
+            else:
+                print(listMoves[0][0], listMoves[0][1])                      
+        else:
+            pass # heuristic  
